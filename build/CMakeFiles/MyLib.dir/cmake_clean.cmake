4c0d567c433fc61f8d7b7891a1590267db5a1e62
Philip Wenkel <philip.sve@hotmail.com>
2020-09-22 11:57:49 +0200

    first

---
+++ build/CMakeFiles/MyLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyLib.dir/my_lib.cpp.o"
+  "libMyLib.dylib"
+  "libMyLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ my_lib.cpp
@@ -0,0 +1,5 @@
+#include "my_lib_api.hpp"
+
+Person::Person(){
+	cout << "Person 1" << endl;
+}
